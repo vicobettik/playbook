7de3b8488a9f990ead2dfec3fb41bba0aaa32bf1
Victor Fuentes <amaury.fuentes@iisi.com.mx>
2022-04-13 01:24:47 -0500

    ejercicios 1-2 de semana 2

---
+++ weekly_mission_2/examples1/example1.js
@@ -0,0 +1,6 @@
+// se define un arreglo de números
+const numbers = [1,2,3, 4, 5];
+
+console.log("Ejemplo 1: Imprimiendo los elementos de un arreglo");
+
+numbers.forEach( num =>  console.log(num) )
---
+++ weekly_mission_2/examples1/example2.js
@@ -0,0 +1,7 @@
+// Ejemplo 2: for Each para calcular la suma de todos los elementos de una lista
+let sum = 0;
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach( num => sum += num );
+console.log("Ejemplo 2: Cálculo de la suma de los elementos de la lista");
+console.log(sum);
