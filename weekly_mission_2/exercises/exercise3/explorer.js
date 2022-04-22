688cc9d6110bf298a0e84f73fb652421e782e7fb
Victor Fuentes <amaury.fuentes@iisi.com.mx>
2022-04-21 20:55:59 -0500

    se agrega ejercicio 3 semana 2

---
+++ weekly_mission_2/exercises/exercise3/explorer.js
@@ -0,0 +1,10 @@
+export default class Explorer {
+    constructor(name, exercises_completed, rate, city, stack, missions){
+        this.name = name;
+        this.exercises_completed = exercises_completed;
+        this.rate = rate;
+        this.city = city;
+        this.stack = stack;
+        this.missions = missions;
+    }
+}
