05f974fe468b14af5d55ef5b9006471a1edcc000
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2022-04-23 20:32:07 -0500

    se agregan geters y seters del proyecto twitter de la  semana 3

--- weekly_mission_3/Twitter/app/models/user.js
+++ weekly_mission_3/Twitter/app/models/user.js
@@ -9,6 +9,30 @@ class User{
         this.lastUpdate = new Date();
     }
 
+    get getUsername() {
+        return this.username;
+    }
+
+    get getBio(){
+        return this.bio;
+    }
+
+    get getDateCreated() {
+        return this.dateCreated;
+    }
+
+    get getLastUpdate() {
+        return this.lastUpdate;
+    }
+
+    set setUsername(newUserName){
+        this.username = newUserName;
+    }
+
+    set setBio(newBio){
+        this.bio = newBio;
+    }
+
 }
 
 module.exports = User;
--- weekly_mission_3/Twitter/test/models/user.test.js
+++ weekly_mission_3/Twitter/test/models/user.test.js
@@ -3,7 +3,7 @@ const User = require('../../app/models/user')
 describe('Unit test for User class', () => {
   
     test('Create an User object', () => {
-      const user = new User(1, "vicobettik", "Victor", "Bio", "dateCreated", "lastUpdate");
+      const user = new User(1, "vicobettik", "Victor", "Bio");
 
       expect(user.id).toBe(1)
       expect(user.username).toBe("vicobettik")
@@ -12,6 +12,25 @@ describe('Unit test for User class', () => {
       expect(user.dateCreated).not.toBeUndefined()
       expect(user.lastUpdate).not.toBeUndefined()
     });
+    
+    test('Add getters', () => {
+      const user = new User(1, "vicobettik", "Victor", "Bio");
+      expect(user.getUsername).toBe("vicobettik")
+      expect(user.getBio).toBe("Bio")
+      expect(user.getDateCreated).not.toBeUndefined()
+      expect(user.getLastUpdate).not.toBeUndefined()
+    });
+
+
+    test('Add setters', () => {
+      const user = new User(1, "vicobettik", "Victor", "Bio");
+      user.setUsername = 'VICOBETTIK';
+      expect(user.username).toBe('VICOBETTIK');
+
+      user.setBio = "bio2";
+      expect(user.bio).toBe("bio2");
+
+    })
     
 
 })
