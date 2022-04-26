a2dba4832937940d9a7860d1b64346f7d1510be8
Victor Fuentes <amaury.fuentes@iisi.com.mx>
2022-04-25 19:57:35 -0500

    se termina primera api usando express

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -16,6 +16,34 @@ app.get( '/v1/explorers', (req, res) => {
     res.status(200).json(explorers);
 });
 
+
+app.get( '/v1/explorers/:id', (req, res) => {
+    console.log(`Api explorers GET request ${new Date()}`);
+    console.log(`Getting explorer with id ${req.params.id}`);
+    const explorer1 = {id: 1, name: 'vic1'};
+    res.status(200).json(explorer1);
+});
+
+app.post( '/v1/explorers', (req, res) => {
+    console.log(`Api explorers POST request ${new Date()}`);
+    const requestBody = req.body;
+    res.status(201).json({message: 'Created'});
+});
+
+app.put( '/v1/explorers/:id', (req, res) =>{
+    console.log(`Api explorers PUT request ${new Date()}`);
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body;
+    res.status(200).json({message: 'Updated'});
+});
+
+app.delete( '/v1/explorers/:id', (req, res) =>{
+    console.log(`Api explorers DELETE request ${new Date()}`);
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body;
+    res.status(200).json({message: 'Deleted'});
+});
+
 app.listen( port, () => {
     console.log(`Example app listening on port ${port}`);
 })
